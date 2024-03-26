d7554006a84551b599b49fce5d1f79bf95c885cc
Raphael Dias <vidadophael@gmail.com>
2024-03-26 17:14:55 -0300

    Added basic 'name' display interactivity.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing === true) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
