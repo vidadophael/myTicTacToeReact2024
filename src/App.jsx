e43df14b7bb8e787b9694fc67ce77a49aad609b7
Raphael Dias <vidadophael@gmail.com>
2024-03-27 14:35:54 -0300

    Capturing the value entered in the input field (player name)

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name = 'Player 1' symbol = 'X' />
-          <Player name = 'Player 2' symbol = 'O' />
+          <Player inicialName="Player 1" symbol="X" />
+          <Player inicialName="Player 2" symbol="O" />
         </ol>
       </div>
       LOG
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
-  const [isEditing, setIsEditing] = useState(false);
+export default function Player({ inicialName, symbol }) {
+  const [ playerName, setPlayerName ] = useState(inicialName);
+
+  const [ isEditing, setIsEditing ] = useState(false);
 
   function handleEditClick() {
-     setIsEditing((editing) => !editing);
+    setIsEditing((editing) => !editing);
   }
 
-  //console.log(isEditing)
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name}/>;
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
