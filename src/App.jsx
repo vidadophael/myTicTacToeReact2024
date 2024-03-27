f96111d09b3ae525f401b601398b054cf91a95f6
Raphael Dias <vidadophael@gmail.com>
2024-03-27 15:25:54 -0300

    Creating game field

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
           <Player inicialName="Player 1" symbol="X" />
           <Player inicialName="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
       LOG
     </main>
